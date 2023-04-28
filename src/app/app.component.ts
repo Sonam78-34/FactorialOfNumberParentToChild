2f2e4bc92c60297c13a625c4a67a70d4b948b9bf
sonam <sonamsinghh919@gmail.com>
2023-04-28 08:05:03 +0530

    Uploading factorial of number  first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'FactorialParentToChild';
+
+  factorialOfNumber(numberOne: number): number 
+  {
+    if (numberOne === 0)
+     {
+      return 1;
+    }
+    else 
+    {
+      return numberOne * this.factorialOfNumber(numberOne - 1);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FactorialComponent } from './ParentToChild/factorial/factorial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactorialComponent
   ],
   imports: [
     BrowserModule,
